3f73631b7870f9b6c9e7ac30f090519f9c3796eb
Shrinath <shrinathrajeshirke@gmail.com>
2025-11-08 16:38:46 +0530

    data validation transformation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation, DataTransformationArtifacts, DataTransformationConfig
 import sys
 if __name__=="__main__":
     try:
@@ -10,9 +12,20 @@ if __name__=="__main__":
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("initiate data ingestion")
-        
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         print(dataingestionartifact)
-
+        logging.info("Data initiation completed")
+        datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact, datavalidationconfig)
+        logging.info("initiate data validation")
+        datavalidationartifact=data_validation.initiate_data_validation()
+        logging.info("data validation completed")
+        print(datavalidationartifact)
+        logging.info("data transformation started")
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(datavalidationartifact,data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("data transformation completed")
+        print(data_transformation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
